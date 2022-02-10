fa05adc3e5b3a91063970373cd9722c4b146a7e3
ikena14 <kevinmccilesta13@gmail.com>
2022-02-10 15:25:23 +0700

    Updating Berpikir

---
+++ berpikir.erl
@@ -0,0 +1,5 @@
+ThingsToBuy = [{apples, 10}, {banana, 6}, {milk, 3}].
+[FirstThing|OtherThingsToBuy] = ThingsToBuy.
+*So Value is=
+    "First Thing is Apples and Other Things as Banana and Milk"
+*
